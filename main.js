1d61573368e76b1a1b946c7a2976c52aedb5ee4f
Gabe Lyu <no6xichengavenue@gmail.com>
2019-07-29 11:04:49 +0100

    Needs refactoring, node list messed up

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 const X = 0;
 const Y = 1;
 const Z = 2;
+const COLOURS = ["#800080", "#ff8000", "#008000"];
+// TODO: Refactor so that functions are property name and dimension count independent
+// TODO: Name: "axis" or "dimension"?
 
 function ds(p1, p2) {
   let dx = p1.a - p2.a;
   let dy = p1.b - p2.b;
   let dz = p1.c - p2.c;
   return Math.sqrt(dx * dx + dy * dy + dz * dz);
+}
+
+function dsAxis(p1, p2, axis) {
+  let dx = p1.a - p2.a;
+  let dy = p1.b - p2.b;
+  let dz = p1.c - p2.c;
+  switch (axis) {
+    case X:
+      return Math.sqrt(dy * dy + dz * dz);
+    case Y:
+      return Math.sqrt(dx * dx + dz * dz);
+    case Z:
+      return Math.sqrt(dy * dy + dx * dx);
+  }
+  throw new Error("Invalid axis");
 }
 
 function randIntBetween(a, b) {
@@ -20,12 +38,13 @@ class PointSystem {
     this.links = [];
     this.visibleNodes = [];
     this.visibleLinks = [];
+    this.beacon = undefined;
     for (let i = 0; i < n; i++) {
       let newPoint = new Point(
         i,
-        randIntBetween(0, 200),
-        randIntBetween(0, 200),
-        randIntBetween(0, 200)
+        randIntBetween(0, 100),
+        randIntBetween(0, 100),
+        randIntBetween(0, 100)
       );
       this.nodes.push(newPoint);
     }
@@ -52,22 +71,82 @@ class PointSystem {
   }
 
   getVisibleLinks(viewNode, dimension, width) {
-    this.visibleNodes = this.nodes.filter(node => ds(viewNode, node) < width);
-    let arr = this.visibleNodes.slice();
+    let visibleCandidates = this.nodes.filter(
+      node => dsAxis(viewNode, node, dimension) < width
+    );
     switch (dimension) {
       case X:
-        arr.sort((a, b) => a.a < b.a);
+        visibleCandidates.sort((p1, p2) => p1.a - p2.a);
         break;
       case Y:
-        arr.sort((a, b) => a.b < b.b);
+        visibleCandidates.sort((p1, p2) => p1.b - p2.b);
         break;
       case Z:
-        arr.sort((a, b) => a.c < b.c);
+        visibleCandidates.sort((p1, p2) => p1.c - p2.c);
         break;
     }
-    for (let i = 0; i < arr.length - 1; i++) {
-      this.visibleLinks.push({ source: arr[i], target: arr[i + 1] });
+    this.visibleNodes.push(visibleCandidates[0]);
+    for (let i = 1; i < visibleCandidates.length; i++) {
+      switch (dimension) {
+        case X:
+          if (
+            this.visibleNodes[this.visibleNodes.length - 1].a !=
+            visibleCandidates[i].a
+          ) {
+            this.visibleNodes.push(visibleCandidates[i]);
+          } else {
+            if (
+              ds(this.visibleNodes[this.visibleNodes.length - 1], viewNode) <
+              ds(visibleCandidates[i], viewNode)
+            ) {
+              this.visibleNodes[this.visibleNodes.length - 1] =
+                visibleCandidates[i];
+            }
+          }
+          break;
+        case Y:
+          if (
+            this.visibleNodes[this.visibleNodes.length - 1].b !=
+            visibleCandidates[i].b
+          ) {
+            this.visibleNodes.push(visibleCandidates[i]);
+          } else {
+            if (
+              ds(this.visibleNodes[this.visibleNodes.length - 1], viewNode) <
+              ds(visibleCandidates[i], viewNode)
+            ) {
+              this.visibleNodes[this.visibleNodes.length - 1] =
+                visibleCandidates[i];
+            }
+          }
+          break;
+        case Z:
+          if (
+            this.visibleNodes[this.visibleNodes.length - 1].c !=
+            visibleCandidates[i].c
+          ) {
+            this.visibleNodes.push(visibleCandidates[i]);
+          } else {
+            if (
+              ds(this.visibleNodes[this.visibleNodes.length - 1], viewNode) <
+              ds(visibleCandidates[i], viewNode)
+            ) {
+              this.visibleNodes[this.visibleNodes.length - 1] =
+                visibleCandidates[i];
+            }
+          }
+          break;
+      }
     }
+    for (let i = 0; i < this.visibleNodes.length - 1; i++) {
+      this.visibleNodes[i].next = this.visibleNodes[i + 1];
+      this.visibleNodes[i + 1].prev = this.visibleNodes[i];
+      this.visibleLinks.push(
+        new Link(this.visibleNodes[i], this.visibleNodes[i + 1])
+      );
+    }
+    this.visibleNodes[0].prev = undefined;
+    this.visibleNodes[this.visibleNodes.length - 1].next = undefined;
   }
 
   getRandomPoint() {
@@ -77,6 +156,7 @@ class PointSystem {
 
   setBeacon(point) {
     point.beacon = true;
+    this.beacon = point;
   }
 }
 
@@ -89,16 +169,212 @@ class Point {
     this.beacon = false;
     this.links = new Array(6);
   }
+
+  render(canvas, x, y, axis) {
+    let width = canvas.width;
+    let height = canvas.height;
+    let renderX = x * width;
+    let renderY = y * height;
+    let c = canvas.getContext("2d");
+    c.beginPath();
+    c.arc(renderX, renderY, 20, 0, Math.PI * 2, false);
+    c.fillStyle = "#ffffff";
+    c.strokeStyle = COLOURS[axis];
+    c.lineWidth = 5;
+    c.fill();
+    c.stroke();
+    c.closePath();
+    c.fillStyle = COLOURS[axis];
+    c.font = "20px Arial";
+    c.fillText(this.a, renderX - 7, renderY + 7);
+  }
 }
 
-system = new PointSystem(200);
-system.setBeacon(system.getRandomPoint());
-location = system.getRandomPoint();
-while (location.beacon) {
-  location = system.getRandomPoint();
+class Link {
+  constructor(sourceId, targetId) {
+    this.source = sourceId;
+    this.target = targetId;
+  }
+
+  render(canvas, sx, sy, tx, ty, axis) {
+    let width = canvas.width;
+    let height = canvas.height;
+    let sourceX = sx * width;
+    let sourceY = sy * height;
+    let targetX = tx * width;
+    let targetY = ty * height;
+    let c = canvas.getContext("2d");
+    c.beginPath();
+    c.moveTo(sourceX, sourceY);
+    c.lineTo(targetX, targetY);
+    c.strokeStyle = COLOURS[axis];
+    c.lineWidth = 5;
+    c.stroke();
+    c.closePath();
+  }
 }
-system.getVisibleLinks(location);
-console.log(location);
-console.log(beacon);
-console.log(system.visibleNodes);
-console.log(system.visibleLinks);
+
+class Game {
+  constructor(n, canvas) {
+    this.radius = 20;
+    this.system = new PointSystem(n);
+    this.system.setBeacon(this.system.getRandomPoint());
+    this.loc = this.system.getRandomPoint();
+    while (this.loc.beacon) {
+      this.loc = this.system.getRandomPoint();
+    }
+    this.axis = X;
+    this.system.getVisibleLinks(this.loc, this.axis, this.radius);
+
+    this.viewRadius = 5;
+    this.canvas = canvas;
+    this.render(this.loc, this.axis, this.viewRadius);
+
+    window.addEventListener("keypress", e => {
+      console.log(e.key);
+      e.preventDefault();
+      switch (e.key) {
+        case "w":
+          this.changeDimUp();
+          break;
+        case "s":
+          this.changeDimDown();
+          break;
+        case "a":
+          this.traverseLeft();
+          break;
+        case "d":
+          this.traverseRight();
+          break;
+      }
+    });
+  }
+
+  traverseRight() {
+    if (this.loc.next) {
+      this.loc = this.loc.next;
+      this.system.getVisibleLinks(this.loc, this.axis, this.radius);
+      this.render(this.loc, this.axis, this.viewRadius);
+    }
+  }
+  traverseLeft() {
+    if (this.loc.prev) {
+      this.loc = this.loc.prev;
+      this.system.getVisibleLinks(this.loc, this.axis, this.radius);
+      this.render(this.loc, this.axis, this.viewRadius);
+    }
+  }
+  changeDimUp() {
+    this.axis = (this.axis + 1) % 3;
+    this.system.getVisibleLinks(this.loc, this.axis, this.radius);
+    this.render(this.loc, this.axis, this.viewRadius);
+  }
+  changeDimDown() {
+    this.axis = (this.axis + 3 - 1) % 3;
+    this.system.getVisibleLinks(this.loc, this.axis, this.radius);
+    this.render(this.loc, this.axis, this.viewRadius);
+  }
+
+  render(loc, axis, radius) {
+    let c = this.canvas.getContext("2d");
+    c.clearRect(0, 270 - 20 - 5, 960, 20 + 5 + 20 + 5);
+    let pointsInView = [];
+    let linksInView = [];
+    let lower = 0;
+    let upper = 0;
+    switch (axis) {
+      case X:
+        lower = loc.a - radius;
+        upper = loc.a + radius;
+        pointsInView = this.system.visibleNodes.filter(
+          node => node.a > lower && node.a < upper
+        );
+        linksInView = this.system.visibleLinks.filter(
+          link =>
+            pointsInView.indexOf(link.source) > -1 ||
+            pointsInView.indexOf(link.target) > -1
+        );
+        linksInView.forEach(link => {
+          link.render(
+            this.canvas,
+            (link.source.a - lower) / (upper - lower),
+            0.5,
+            (link.target.a - lower) / (upper - lower),
+            0.5,
+            axis
+          );
+        });
+        pointsInView.forEach(point => {
+          point.render(
+            this.canvas,
+            (point.a - lower) / (upper - lower),
+            0.5,
+            axis
+          );
+        });
+        break;
+      case Y:
+        lower = loc.b - radius;
+        upper = loc.b + radius;
+        pointsInView = this.system.visibleNodes.filter(
+          node => node.b > lower && node.b < upper
+        );
+        linksInView = this.system.visibleLinks.filter(
+          link =>
+            pointsInView.indexOf(link.source) > -1 ||
+            pointsInView.indexOf(link.target) > -1
+        );
+        linksInView.forEach(link => {
+          link.render(
+            this.canvas,
+            (link.source.b - lower) / (upper - lower),
+            0.5,
+            (link.target.b - lower) / (upper - lower),
+            0.5,
+            axis
+          );
+        });
+        pointsInView.forEach(point => {
+          point.render(
+            this.canvas,
+            (point.b - lower) / (upper - lower),
+            0.5,
+            axis
+          );
+        });
+        break;
+      case Z:
+        lower = loc.c - radius;
+        upper = loc.c + radius;
+        pointsInView = this.system.visibleNodes.filter(
+          node => node.c > lower && node.c < upper
+        );
+        linksInView = this.system.visibleLinks.filter(
+          link =>
+            pointsInView.indexOf(link.source) > -1 ||
+            pointsInView.indexOf(link.target) > -1
+        );
+        linksInView.forEach(link => {
+          link.render(
+            this.canvas,
+            (link.source.c - lower) / (upper - lower),
+            0.5,
+            (link.target.c - lower) / (upper - lower),
+            0.5,
+            axis
+          );
+        });
+        pointsInView.forEach(point => {
+          point.render(
+            this.canvas,
+            (point.c - lower) / (upper - lower),
+            0.5,
+            axis
+          );
+        });
+        break;
+    }
+  }
+}
+
+let game = new Game(200, document.getElementById("canvas"));
